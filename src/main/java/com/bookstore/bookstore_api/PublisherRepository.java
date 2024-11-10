9fbde90fcf591aa4058a9b6ed060d30c6943a0a6
Danielle Martin <139602254+danim53@users.noreply.github.com>
2024-11-10 12:58:21 -0800

    added apply discount percent to publisher name with PUT request

--- src/main/java/com/bookstore/bookstore_api/PublisherRepository.java
+++ src/main/java/com/bookstore/bookstore_api/PublisherRepository.java
@@ -1,9 +1,11 @@
 package com.bookstore.bookstore_api;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
-    Publisher findByName(String name);
+    Optional<Publisher> findByName(String name);
 }
